0eb5a84b11c5309278331f87b1035a1794ff4e8e
JoseArmado <jose_armando_g.v@hotmail.com>
2016-08-09 22:00:02 -0700

    iconos etc

--- app/js/config.js
+++ app/js/config.js
@@ -1,6 +1,6 @@
 materialAdmin
     .config(function ($stateProvider, $urlRouterProvider){
-        $urlRouterProvider.otherwise("/home");
+        $urlRouterProvider.otherwise("/maestros");
 
         $stateProvider
             
@@ -20,5 +20,45 @@ materialAdmin
             .state ('preHorarios', {
                 url: '/preHorarios',
                 templateUrl: 'views/preHorarios.html'
+            })
+            .state ('sistemas', {
+                url: '/sistemas',
+                templateUrl: 'views/PreHorarios/sistemas.html'
+            })
+            .state ('electrica', {
+                url: '/electrica',
+                templateUrl: 'views/PreHorarios/electrica.html'
+            })
+            .state ('electronica', {
+                url: '/electronica',
+                templateUrl: 'views/PreHorarios/electronica.html'
+            })
+            .state ('mecanica', {
+                url: '/mecanica',
+                templateUrl: 'views/PreHorarios/mecanica.html'
+            })
+            .state ('mecatronica', {
+                url: '/mecatronica',
+                templateUrl: 'views/PreHorarios/mecatronica.html'
+            })
+            .state ('administracion', {
+                url: '/administracion',
+                templateUrl: 'views/PreHorarios/administracion.html'
+            })
+            .state ('gestion', {
+                url: '/gestion',
+                templateUrl: 'views/PreHorarios/gestion.html'
+            })
+            .state ('informatica', {
+                url: '/informatica',
+                templateUrl: 'views/PreHorarios/informatica.html'
+            })
+            .state ('industrial', {
+                url: '/industrial',
+                templateUrl: 'views/PreHorarios/industrial.html'
+            })
+            .state ('biomedica', {
+                url: '/biomedica',
+                templateUrl: 'views/PreHorarios/biomedica.html'
             })
     });
