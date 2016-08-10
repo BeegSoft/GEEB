332ce7b1bcf8a96d20efe193fa45959af951fdb8
CarlosAlatorre <carlos_alatorre_95@hotmail.com>
2016-08-09 22:21:59 -0700

    Arreglo de conflictos

--- app/js/config.js
+++ app/js/config.js
@@ -6,12 +6,9 @@ materialAdmin
             
             .state ('maestros', {
                 url: '/maestros',
-                templateUrl: 'views/maestros.html',
+                templateUrl: 'views/maestros.html'
             })
-            .state ('home', {
-                url: '/home',
-                templateUrl: 'views/home.html',
-            })
+          
 
             .state ('materias', {
                 url: '/materias',
@@ -19,7 +16,7 @@ materialAdmin
             })
             .state ('sugerencias', {
                 url: '/sugerencias',
-                templateUrl: 'views/sugerencias.html',
+                templateUrl: 'views/sugerencias.html'
             })
             .state ('preHorarios', {
                 url: '/preHorarios',
@@ -65,7 +62,7 @@ materialAdmin
             .state ('biomedica', {
                 url: '/biomedica',
                 templateUrl: 'views/PreHorarios/biomedica.html'
-
+            })
 
             .state ('pages', {
                 url: '/pages',
@@ -80,12 +77,12 @@ materialAdmin
         
             .state ('pages.profile.profile-comentarios', {
                 url: '/profile-comentarios',
-                templateUrl: 'views/profile-comentarios.html',
+                templateUrl: 'views/profile-comentarios.html'
             })
 
             .state ('pages.profile.profile-preguntas', {
                 url: '/profile-preguntas',
-                templateUrl: 'views/profile-preguntas.html',
+                templateUrl: 'views/profile-preguntas.html'
             })
 
             .state ('pages.home', {
@@ -95,16 +92,16 @@ materialAdmin
         
             .state ('pages.home.home-vistos', {
                 url: '/home-vistos',
-                templateUrl: 'views/home-vistos.html',
+                templateUrl: 'views/home-vistos.html'
             })
 
             .state ('pages.home.home-comentados', {
                 url: '/home-comentados',
-                templateUrl: 'views/home-comentados.html',
+                templateUrl: 'views/home-comentados.html'
             })
             .state ('pages.home.home-valorados', {
                 url: '/home-valorados',
-                templateUrl: 'views/home-valorados.html',
+                templateUrl: 'views/home-valorados.html'
             })
         
             //-------------------------------
@@ -136,27 +133,6 @@ materialAdmin
                                 
             .state ('pages.wall', {
                 url: '/wall',
-                templateUrl: 'views/wall.html',
-                resolve: {
-                    loadPlugin: function($ocLazyLoad) {
-                        return $ocLazyLoad.load ([
-                            {
-                                name: 'vendors',
-                                insertBefore: '#app-level',
-                                files: [
-                                    'vendors/bower_components/autosize/dist/autosize.min.js',
-                                    'vendors/bower_components/lightgallery/light-gallery/css/lightGallery.css'
-                                ]
-                            },
-                            {
-                                name: 'vendors',
-                                files: [
-                                    'vendors/bower_components/mediaelement/build/mediaelement-and-player.js',
-                                    'vendors/bower_components/lightgallery/light-gallery/js/lightGallery.min.js'
-                                ]
-                            }
-                        ])
-                    }
-                }
+                templateUrl: 'views/wall.html'
             })
     });
